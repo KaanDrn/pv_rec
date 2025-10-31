Fix.gpu data leakage (#2)
* Changed some things to make the package more usefull for different data types

* bumped version

* bumped version again as rc

* Fixed the data leakage issue, when running the model too often

---------

Co-authored-by: Kaan Duran <k35400@eon.com>

pv_rec/ml_lib.py
@@ -41,10 +41,11 @@ class AbstractProductEncoder(_HuggingFaceConnection):
         if self.model is None:
             self.model = SentenceTransformer(self.model_name)
 
-        embeddings = self.model.encode(
-            texts,
-            show_progress_bar=True
-        )
+        with torch.no_grad():
+            embeddings = self.model.encode(
+                texts,
+                show_progress_bar=True
+            )
         embeddings_df = pd.DataFrame(
             embeddings,
             index=texts,